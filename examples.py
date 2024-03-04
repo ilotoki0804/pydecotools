ef4f0ec394853bbfcfbc4474e571abd7a2659b31
ilotoki0804 <ilotoki0804@gmail.com>
2024-03-04 12:59:18 +0900

    Update examples

--- examples.py
+++ examples.py
@@ -3,13 +3,13 @@ from src.decotools.applier import *
 from src.decotools.itertools import *
 from src.decotools.utilities import *
 
-10 @range @map.partial(lambda x: x + 1) @list @print
+print(10 @range @map.partial(lambda x: x + 1) @list)
 # without decorator: print(list(map(lambda x: x + 1, range(10))))
 
-100 @range @map.partial(lambda x: x ** 2) @filter.partial(lambda x: x % 7 != 0) @list @str @bytes.partial(encoding="utf-8") @list @sum @print
+print(100 @range @map.partial(lambda x: x ** 2) @filter.partial(lambda x: x % 7 != 0) @list @str @bytes.partial(encoding="utf-8") @list @sum)
 # without decorator: print(sum(list(bytes(str(list(filter(lambda x: x % 7 != 0, map(lambda x: x ** 2, range(100))))), encoding="utf-8"))))
 
-100 @range @list @map.partial(str) @decorator @" | ".join @print
+print(100 @range @list @map.partial(str) @decorator(" | ".join))
 # without decorator: print(" | ".join(map(str, list(range(100)))))
 
-"hello world" @getattr.supply("split") @call() @print
+print("hello world" @getattr.supply("split") @call())
